981a85db242681327857ab04dc5ddbcf27a430be
shaik aqsa fathima <shaikaqsa.78@gmail.com>
2024-12-15 11:07:15 +0530

    Add files via upload

---
+++ alive.java
@@ -0,0 +1,9 @@
+class Main{
+    public static void main(String args[])
+    {
+        Runnable r=()->System.out.println("im new from java8 version");
+        Thread t=new Thread(r);
+        t.start();
+        System.out.println(t.isAlive());
+    }
+}
